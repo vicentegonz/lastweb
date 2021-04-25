1456a909b4726f544284cdb2a487c0a6ed2c3981
quememo <48305214+quememo@users.noreply.github.com>
2021-04-25 15:47:58 -0400

    Added Ant Design to the project (#5)
    * Added Ant Design to the project
    
    * Added really basic /admin view using Ant Design features

---
+++ pages/admin/index.jsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import React from 'react';
+import {
+  Row, Col, Avatar, Button,
+} from 'antd';
+import 'antd/dist/antd.css';
+import Layout, { siteTitle } from '@/components/layout/layout.jsx';
+
+export default function Home() {
+  return (
+    <>
+      <Layout home>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+      </Layout>
+
+      <>
+        <Row justify="center">
+          <Col span={4}>
+            <Avatar style={{ backgroundColor: '#f56a00', verticalAlign: 'middle' }} size="large">
+              Icono
+            </Avatar>
+          </Col>
+          <Col span={12}>
+            <Button type="primary">Tienda N°1</Button>
+          </Col>
+        </Row>
+      </>
+
+    </>
+  );
+}
