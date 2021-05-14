1358981cd764c23d86931ed55a7bdef3c872ebb2
quememo <48305214+quememo@users.noreply.github.com>
2021-05-14 17:01:56 -0400

    🚩Added Reports wireframe page for Admin (#18)
    * Added Reports wireframe page for Admin
    
    * - Refactored DummyChart to make it more reusable
    
    * - Added fries and drinks card
    * - Improved CSS spacing of cards and dummy chart

---
+++ components/admin/reports/DummyChart.jsx
@@ -0,0 +1,110 @@
+import { ResponsiveBar } from '@nivo/bar';
+import {
+  string, arrayOf, objectOf, number,
+} from 'prop-types';
+
+const DummyChart = ({ chartIndex, chartKeys, chartData }) => (
+  <ResponsiveBar
+    indexBy={chartIndex}
+    keys={chartKeys}
+    data={chartData}
+    margin={{
+      top: 50, right: 130, bottom: 50, left: 60,
+    }}
+    padding={0.3}
+    valueScale={{ type: 'linear' }}
+    indexScale={{ type: 'band', round: true }}
+    colors={{ scheme: 'nivo' }}
+    defs={[
+      {
+        id: 'dots',
+        type: 'patternDots',
+        background: 'inherit',
+        color: '#38bcb2',
+        size: 4,
+        padding: 1,
+        stagger: true,
+      },
+      {
+        id: 'lines',
+        type: 'patternLines',
+        background: 'inherit',
+        color: '#eed312',
+        rotation: -45,
+        lineWidth: 6,
+        spacing: 10,
+      },
+    ]}
+    fill={[
+      {
+        match: {
+          id: chartKeys[4],
+        },
+        id: 'dots',
+      },
+      {
+        match: {
+          id: chartKeys[2],
+        },
+        id: 'lines',
+      },
+    ]}
+    borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'Tienda',
+      legendPosition: 'middle',
+      legendOffset: 32,
+    }}
+    axisLeft={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'Comida Vendida',
+      legendPosition: 'middle',
+      legendOffset: -40,
+    }}
+    labelSkipWidth={12}
+    labelSkipHeight={12}
+    labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+    legends={[
+      {
+        dataFrom: 'keys',
+        anchor: 'bottom-right',
+        direction: 'column',
+        justify: false,
+        translateX: 120,
+        translateY: 0,
+        itemsSpacing: 2,
+        itemWidth: 100,
+        itemHeight: 20,
+        itemDirection: 'left-to-right',
+        itemOpacity: 0.85,
+        symbolSize: 20,
+        effects: [
+          {
+            on: 'hover',
+            style: {
+              itemOpacity: 1,
+            },
+          },
+        ],
+      },
+    ]}
+    animate
+    motionStiffness={90}
+    motionDamping={15}
+  />
+);
+
+DummyChart.propTypes = {
+  chartIndex: string.isRequired,
+  chartKeys: arrayOf(string).isRequired,
+  chartData: arrayOf(objectOf(number)).isRequired,
+};
+
+export default DummyChart;
---
+++ components/admin/reports/Reports.jsx
@@ -0,0 +1,144 @@
+import 'antd/dist/antd.css';
+import {
+  Row, Col, Typography, Divider, DatePicker, Space, Card,
+} from 'antd';
+
+import DummyChart from './DummyChart.jsx';
+import { dummyChartIndex, dummyChartKeys, dummyChartData } from './dummyChartData.jsx';
+
+import reportsStyles from './Reports.module.scss';
+
+const { Title } = Typography;
+
+const AdminReports = () => (
+  <div className={reportsStyles.containerHorizontalMargin}>
+
+    <Row justify="space-between">
+
+      <Col>
+        <Title level={2}>
+          Reportes
+        </Title>
+      </Col>
+      <Col span={4}>
+        <DatePicker className={reportsStyles.fatherWidth} placeholder="Seleccione una fecha" format="DD/MM/YYYY" />
+      </Col>
+    </Row>
+
+    <div>
+      <Divider className={reportsStyles.dividerA} />
+    </div>
+
+    <Row gutter={32}>
+      <Col span={10}>
+        <Space direction="vertical" size="large" className={reportsStyles.fatherWidth}>
+          <Row justify="space-around">
+
+            <Col span={10}>
+              <Card
+                title={(
+                  <div>
+                    <div>Total donuts</div>
+                    <div>vendidas</div>
+                  </div>
+                )}
+                className={reportsStyles.foodCard}
+              >
+                <Title type="success">628</Title>
+              </Card>
+            </Col>
+
+            <Col span={10}>
+              <Card
+                title={(
+                  <div>
+                    <div>Total sandwichs</div>
+                    <div>vendidos</div>
+                  </div>
+                )}
+                className={reportsStyles.foodCard}
+              >
+                <Title type="success">825</Title>
+              </Card>
+            </Col>
+
+          </Row>
+
+          <Row justify="space-around">
+
+            <Col span={10}>
+              <Card
+                title={(
+                  <div>
+                    <div>Total hot dogs</div>
+                    <div>vendidos</div>
+                  </div>
+                )}
+                className={reportsStyles.foodCard}
+              >
+                <Title type="success">434</Title>
+              </Card>
+            </Col>
+
+            <Col span={10}>
+              <Card
+                title={(
+                  <div>
+                    <div>Total burgers</div>
+                    <div>vendidas</div>
+                  </div>
+                )}
+                className={reportsStyles.foodCard}
+              >
+                <Title type="success">505</Title>
+              </Card>
+            </Col>
+
+          </Row>
+
+          <Row justify="space-around">
+            <Col span={10}>
+              <Card
+                title={(
+                  <div>
+                    <div>Total papas fritas</div>
+                    <div>vendidas</div>
+                  </div>
+                )}
+                className={reportsStyles.foodCard}
+              >
+                <Title type="success">580</Title>
+              </Card>
+            </Col>
+
+            <Col span={10}>
+              <Card
+                title={(
+                  <div>
+                    <div>Total bebidas</div>
+                    <div>vendidas</div>
+                  </div>
+                )}
+                className={reportsStyles.foodCard}
+              >
+                <Title type="success">628</Title>
+              </Card>
+            </Col>
+
+          </Row>
+
+        </Space>
+      </Col>
+      <Col span={14} className={reportsStyles.barChartSize}>
+        <DummyChart
+          chartIndex={dummyChartIndex}
+          chartKeys={dummyChartKeys}
+          chartData={dummyChartData}
+        />
+      </Col>
+    </Row>
+
+  </div>
+);
+
+export default AdminReports;
